8f16665ea684688da68911b6fa5cf7960c855905
Ciro Cattuto <ciro.cattuto@gmail.com>
2025-08-15 09:53:21 +0200

    remove bg color

--- src/peripheral.v
+++ src/peripheral.v
@@ -39,11 +39,11 @@ module tqvp_example (
     localparam CHARS_ADDR_WIDTH = $clog2(NUM_CHARS);
 
     localparam [1:0] DEFAULT_TEXT_COLOR = 2'b00;
-    localparam [5:0] DEFAULT_BG_COLOR = 6'b000000;
+    //localparam [5:0] DEFAULT_BG_COLOR = 6'b000000;
 
     // Text buffer (printable ASCII code in the lowest 7 bits, color in the top 2 bits)
     reg [8:0] text[0:NUM_CHARS-1];
-    reg [5:0] bg_color;
+    //reg [5:0] bg_color;
 
     // ----- HOST INTERFACE -----
     
@@ -55,12 +55,12 @@ module tqvp_example (
     // byte writes use the default text color, wider writes also provide color bits
     wire [1:0] next_color = (data_write_n == 2'b00) ? DEFAULT_TEXT_COLOR : data_in[9:8];
 
-    always @(posedge clk) begin
-        if (!rst_n)
-            bg_color <= DEFAULT_BG_COLOR;
-        else if (we_bg)
-            bg_color <= data_in[5:0];
-    end
+    // always @(posedge clk) begin
+    //     if (!rst_n)
+    //         bg_color <= DEFAULT_BG_COLOR;
+    //     else if (we_bg)
+    //         bg_color <= data_in[5:0];
+    // end
 
     // Handle writes to character/color registers
     always @(posedge clk) begin
@@ -127,53 +127,26 @@ module tqvp_example (
 
     // Character pixels are 8x8 squares in the VGA frame.
     // Character glyphs are 5x7 and padded in a 6x8 character box.
- 
-    // char_x is column of current character in the NUM_ROWS x NUM_COLS text buffer,
-    // rel_x is current pixel's x coordinate within current character.
-    // We use counters to avoid divisions and remainders.
 
-    reg [2:0] rel_x;
-    wire rel_x_5 = (rel_x == 3'd5);
-    reg [2:0] cnt1;
+    // x position machinery
+    reg [5:0] cx48;
     reg [COLS_ADDR_WIDTH-1:0] char_x;
-    
+    wire [2:0] rel_x = cx48[5:3];
+    wire rel_x_5 = (rel_x == 3'd5);
+
     always @(posedge clk) begin
         if (&pix_x_diff) begin  // pix_x == VGA_FRAME_XMIN - 1
-            rel_x <= 0;
-            cnt1 <= 0;
+            cx48 <= 0;
             char_x <= 0;
         end else begin
-            cnt1 <= cnt1 + 1;
-            if (&cnt1) begin
-                if (rel_x_5) begin
-                    rel_x <= 0;
-                    char_x <= char_x + 1;
-                end else begin
-                    rel_x <= rel_x + 1;
-                end
+            if (cx48 == 47) begin
+                cx48 <= 0;
+                char_x <= char_x + 1;
+            end else begin
+                cx48 <= cx48 + 1;
             end
         end
     end
-
-    // reg [5:0] cx48;
-    // reg [COLS_ADDR_WIDTH-1:0] char_x;
-
-    // always @(posedge clk) begin
-    //     if (&pix_x_diff) begin  // pix_x == VGA_FRAME_XMIN - 1
-    //         cx48 <= 0;
-    //         char_x <= 0;
-    //     end else begin
-    //         if (cx48 == 47) begin
-    //             cx48 <= 0;
-    //             char_x <= char_x + 1;
-    //         end else begin
-    //             cx48 <= cx48 + 1;
-    //         end
-    //     end
-    // end
-
-    // wire [2:0] rel_x = cx48[5:3];
-    // wire rel_x_5 = (rel_x == 3'd5);
 
     // Row of current character in the NUM_ROWS x NUM_COLS text buffer
     wire [ROWS_ADDR_WIDTH-1:0] char_y = pix_y_frame[3+ROWS_ADDR_WIDTH-1:3];  // divide by 64 (VGA char height is 64 pixels)
@@ -198,7 +171,8 @@ module tqvp_example (
     wire pixel_on = frame_active & char_pixel;
     wire [2:0] pixel_color = color_rom[char_color];
     wire [5:0] char_bgr = { {2{pixel_color[2]}}, {2{pixel_color[1]}}, {2{pixel_color[0]}} };
-    assign {B, G, R} = ~video_active ? 6'b0 : (pixel_on ? char_bgr : bg_color);
+    //assign {B, G, R} = ~video_active ? 6'b0 : (pixel_on ? char_bgr : bg_color);
+    assign {B, G, R} = (video_active & pixel_on) ? char_bgr : 6'b0;
 
 
     // ----- CHARACTER ROM -----
