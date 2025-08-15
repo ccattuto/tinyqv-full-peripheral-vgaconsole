b594f2ebe6e0f440ec5faf3b44caa13d7fceeca8
Ciro Cattuto <ciro.cattuto@gmail.com>
2025-08-15 08:52:17 +0200

    reducing area #2

--- src/peripheral.v
+++ src/peripheral.v
@@ -132,48 +132,48 @@ module tqvp_example (
     // rel_x is current pixel's x coordinate within current character.
     // We use counters to avoid divisions and remainders.
 
-    // reg [2:0] rel_x;
-    // wire rel_x_5 = (rel_x == 3'd5);
-    // reg [2:0] cnt1;
-    // reg [COLS_ADDR_WIDTH-1:0] char_x;
-    
-    // always @(posedge clk) begin
-    //     if (&pix_x_diff) begin  // pix_x == VGA_FRAME_XMIN - 1
-    //         rel_x <= 0;
-    //         cnt1 <= 0;
-    //         char_x <= 0;
-    //     end else begin
-    //         cnt1 <= cnt1 + 1;
-    //         if (&cnt1) begin
-    //             if (rel_x_5) begin
-    //                 rel_x <= 0;
-    //                 char_x <= char_x + 1;
-    //             end else begin
-    //                 rel_x <= rel_x + 1;
-    //             end
-    //         end
-    //     end
-    // end
-
-    reg [5:0] cx48;
+    reg [2:0] rel_x;
+    wire rel_x_5 = (rel_x == 3'd5);
+    reg [2:0] cnt1;
     reg [COLS_ADDR_WIDTH-1:0] char_x;
-
+    
     always @(posedge clk) begin
         if (&pix_x_diff) begin  // pix_x == VGA_FRAME_XMIN - 1
-            cx48 <= 0;
+            rel_x <= 0;
+            cnt1 <= 0;
             char_x <= 0;
         end else begin
-            if (cx48 == 47) begin
-                cx48 <= 0;
-                char_x <= char_x + 1;
-            end else begin
-                cx48 <= cx48 + 1;
+            cnt1 <= cnt1 + 1;
+            if (&cnt1) begin
+                if (rel_x_5) begin
+                    rel_x <= 0;
+                    char_x <= char_x + 1;
+                end else begin
+                    rel_x <= rel_x + 1;
+                end
             end
         end
     end
 
-    wire [2:0] rel_x = cx48[5:3];
-    wire rel_x_5 = (rel_x == 3'd5);
+    // reg [5:0] cx48;
+    // reg [COLS_ADDR_WIDTH-1:0] char_x;
+
+    // always @(posedge clk) begin
+    //     if (&pix_x_diff) begin  // pix_x == VGA_FRAME_XMIN - 1
+    //         cx48 <= 0;
+    //         char_x <= 0;
+    //     end else begin
+    //         if (cx48 == 47) begin
+    //             cx48 <= 0;
+    //             char_x <= char_x + 1;
+    //         end else begin
+    //             cx48 <= cx48 + 1;
+    //         end
+    //     end
+    // end
+
+    // wire [2:0] rel_x = cx48[5:3];
+    // wire rel_x_5 = (rel_x == 3'd5);
 
     // Row of current character in the NUM_ROWS x NUM_COLS text buffer
     wire [ROWS_ADDR_WIDTH-1:0] char_y = pix_y_frame[3+ROWS_ADDR_WIDTH-1:3];  // divide by 64 (VGA char height is 64 pixels)
