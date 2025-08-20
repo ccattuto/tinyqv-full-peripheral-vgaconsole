f9113dbcf48a06c46da9d92225e9c9865c768a63
Ciro Cattuto <ciro.cattuto@gmail.com>
2025-08-20 22:35:19 +0200

    new char_x safety attempt

--- src/peripheral.v
+++ src/peripheral.v
@@ -145,12 +145,12 @@ module tqvp_example (
     end
 
     // (x,y) character coordinates in NUM_ROWS x NUM_COLS text buffer
-    wire [3:0] char_y = y_blk - 4'd1;
+    wire [1:0] char_y = y_blk[1:0] - 2'd1;
 
     // Drive character ROM input
     //wire [6:0] char_index = text[char_y * NUM_COLS + char_x];
-    wire [4:0] char_addr = ({3'd0, char_y[1:0]} << 3) + ({3'd0, char_y[1:0]} << 1) + char_x;  // we hardcode NUM_COLS = 10, NUM_ROWS=2 to save gates
-    wire [4:0] char_addr_safe = (char_addr == 5'd10) ? 5'd0 : char_addr;
+    wire [3:0] char_x_safe = (char_x == 4'd10) ? 4'd0 : char_x;
+    wire [4:0] char_addr = ({3'd0, char_y} << 3) + ({3'd0, char_y} << 1) + char_x_safe;  // we hardcode NUM_COLS = 10, NUM_ROWS=2 to save gates
 
     wire [6:0] char_index;
     wire [1:0] char_color_index;
