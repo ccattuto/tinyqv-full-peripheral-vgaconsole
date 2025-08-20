3422bca11bdb29c1b4b0b94e103b2ef5ea914fd2
Ciro Cattuto <ciro.cattuto@gmail.com>
2025-08-20 18:45:22 +0200

    full revert

--- src/peripheral.v
+++ src/peripheral.v
@@ -149,8 +149,8 @@ module tqvp_example (
 
     // Drive character ROM input
     //wire [6:0] char_index = text[char_y * NUM_COLS + char_x];
-    wire [COLS_ADDR_WIDTH-1:0] char_x_safe = (char_x == 4'd10) ? 4'd0 : char_x;
-    wire [4:0] char_addr = ({3'd0, char_y[1:0]} << 3) + ({3'd0, char_y[1:0]} << 1) + char_x_safe;  // we hardcode NUM_COLS = 10, NUM_ROWS=2 to save gates
+    wire [COLS_ADDR_WIDTH-1:0] char_x_clamped = (char_x == 4'd10) ? 4'd0 : char_x;
+    wire [4:0] char_addr = ({3'd0, char_y[1:0]} << 3) + ({3'd0, char_y[1:0]} << 1) + char_x_clamped;  // we hardcode NUM_COLS = 10, NUM_ROWS=2 to save gates
 
     wire [6:0] char_index;
     wire [1:0] char_color_index;
